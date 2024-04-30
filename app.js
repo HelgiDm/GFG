154b02641f214dfcb4ede9a60514eec4023b618f
HelgiDm <alpert98@mail.ru>
2024-04-30 22:50:40 +0300

    Making carousel

---
+++ app.js
@@ -0,0 +1,8 @@
+// Appearing of side buttons when hovering over the carousel
+// let sideBtnBox = document.querySelector('.side-btn');
+// // let sideBtnDiv = document.querySelector('.side-btn div');
+// let carouselImg = document.querySelector('.carousel .images');
+
+// carouselImg.addEventListener('mouseover', () => {
+//     sideBtnBox.style.width = '100%';
+// })
