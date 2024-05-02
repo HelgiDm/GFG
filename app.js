a1e9d926c39514d79a1f890fce243a14c8b9eb7f
HelgiDm <alpert98@mail.ru>
2024-05-02 10:43:51 +0300

    Made carousel (JS)

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // Carousel
-let crslBtnLeft = document.querySelector('#side-btn-left');
-let crslBtnRight = document.querySelector('#side-btn-right')
+const crslBtnLeft = document.querySelector('#side-btn-left');
+const crslBtnRight = document.querySelector('#side-btn-right')
 let crslSpecial = document.querySelector('#crsl-special');
 let crslImages = document.querySelector('.images');
+let crslControlBtn = document.querySelector('.control-btn');
+let crslControlBtnSpecial = document.querySelector('#crsl-control-special');
 
-
+// Carousel Side Buttons
 crslBtnLeft.addEventListener('click', () => {
     if (crslSpecial.previousElementSibling) {
         crslSpecial.previousElementSibling.id = 'crsl-special';
@@ -16,6 +18,9 @@ crslBtnLeft.addEventListener('click', () => {
         crslSpecial.id = '';
         crslSpecial = document.querySelector('#crsl-special');
     }
+    crslControlBtn.children[Array.from(crslImages.children).indexOf(crslSpecial)].id = 'crsl-control-special';
+    crslControlBtnSpecial.id = '';
+    crslControlBtnSpecial = document.querySelector('#crsl-control-special');
 });
 
 crslBtnRight.addEventListener('click', () => {
@@ -29,6 +34,24 @@ crslBtnRight.addEventListener('click', () => {
         crslSpecial.id = '';
         crslSpecial = document.querySelector('#crsl-special');
     }
+    crslControlBtn.children[Array.from(crslImages.children).indexOf(crslSpecial)].id = 'crsl-control-special';
+    crslControlBtnSpecial.id = '';
+    crslControlBtnSpecial = document.querySelector('#crsl-control-special');
 })
 
+// Carousel Control Buttons
+let children = crslControlBtn.children;
+for (let child of children) {
+    child.addEventListener('click', () => {
+        child.id = 'crsl-control-special';
+        crslControlBtnSpecial.id = '';
+        crslControlBtnSpecial = document.querySelector('#crsl-control-special');
+        crslImages.children[Array.from(children).indexOf(crslControlBtnSpecial)].id = 'crsl-special';
+        crslSpecial.id = '';
+        crslSpecial = document.querySelector('#crsl-special');
+    });
 
+
+
+
+}
